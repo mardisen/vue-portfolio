0a3e65777a432fb11c42625787c28700def2836e
mardisen <marino.didio.work@gmail.com>
2023-01-24 16:28:40 +0100

    Improved background colors and animation

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,23 +1,18 @@
-<script setup lang="ts">
-import HideoutPattern from "@components/icons/HideoutPattern.vue";
-</script>
-
 <template>
-    <div class="absolute w-screen h-screen -z-10">
+    <div class="overflow-hidden absolute w-screen h-screen -z-10">
         <div class="absolute z-10 w-full h-full">
-            <div class="w-full h-2/5 bg-primary-dark">
+            <div class="w-full h-2/3 bg-primary-darkest">
             </div>
-            <div class="w-full h-3/5 bg-gradient-to-b from-primary-dark">
+            <div class="w-full h-full bg-gradient-to-b from-primary-darkest">
             </div>
         </div>
-        <div class="absolute w-full h-full fill-primary-light bg-primary-dark pattern">
-            <HideoutPattern />
+        <div class="absolute w-[120%] h-[120%] bg-primary-darkest pattern animate-to-t">
         </div>
     </div>
 </template>
 
 <style scoped>
-/* .pattern {
-    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='40' height='40' viewBox='0 0 40 40'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%23d1cdb7' fill-opacity='1'%3E%3Cpath d='M0 38.59l2.83-2.83 1.41 1.41L1.41 40H0v-1.41zM0 1.4l2.83 2.83 1.41-1.41L1.41 0H0v1.41zM38.59 40l-2.83-2.83 1.41-1.41L40 38.59V40h-1.41zM40 1.41l-2.83 2.83-1.41-1.41L38.59 0H40v1.41zM20 18.6l2.83-2.83 1.41 1.41L21.41 20l2.83 2.83-1.41 1.41L20 21.41l-2.83 2.83-1.41-1.41L18.59 20l-2.83-2.83 1.41-1.41L20 18.59z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
-} */
+.pattern {
+    background-image: url("@assets/hideout.svg");
+}
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,13 @@ module.exports = {
     extend: {
       colors: {
         primary: {
-          light: "#d1cdb7",
-          dark: "#302F2C"
+          // light: "#d1cdb7",
+          // dark: "#302F2C"
+          lightest: "#E8EDDF",
+          light: "#CFDBD5",
+          accent: "#F5CB5C",
+          dark: "#333533",
+          darkest: "#242423",
         }
       }
     },
