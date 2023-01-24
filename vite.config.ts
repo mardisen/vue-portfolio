226bb5764a3dc7cb63e199eb4222ca2879178828
mardisen <marino.didio.work@gmail.com>
2023-01-24 12:59:03 +0100

    Added path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
+      "@components": fileURLToPath(new URL("./src/components", import.meta.url)),
+      "@views": fileURLToPath(new URL("./src/views", import.meta.url)),
     },
   },
 });
