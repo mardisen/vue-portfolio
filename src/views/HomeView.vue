99ad100df9a949d43d8e4c02b80d836fa6a00dc3
mardisen <marino.didio.work@gmail.com>
2023-04-05 11:54:01 +0200

    Minor UI improvements and refactoring

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  <main>
-    
-  </main>
+  <div class="flex justify-between items-center h-2/3 home">
+    <div class="text-xl text-left basis-1/3">
+      <code>-</code> Hi this is<br>
+      <code>-</code> My home page<br>
+      <code>-</code> Hello<br>
+    </div>
+    <div class="w-0.5 h-full rounded bg-primary-dark"></div>
+    <div class="text-lg text-right basis-1/3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem aliquid
+      quae quaerat asperiores error? Excepturi, laudantium nulla natus culpa facere laborum? Labore asperiores deleniti
+      atque, enim eligendi esse voluptas ab?</div>
+  </div>
 </template>
+
