82979c75021b3d45227002ae0965cb8eed4dc840
mardisen <marino.didio.work@gmail.com>
2023-04-08 12:43:16 +0200

    Improved Content UI

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,11 +1,5 @@
 <template>
-    <div class="overflow-hidden absolute w-screen h-screen -z-10 bg-primary-darkest">
-        <div class="absolute z-10 w-full h-full">
-            <div class="w-full h-full cover-gradient">
-            </div>
-        </div>
-        <div class="absolute w-[120%] h-[120%] bg-primary-accent pattern animate-to-t">
-        </div>
+    <div class="overflow-hidden absolute w-screen h-screen -z-10 bg-zinc-800">
     </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,32 @@
 <template>
-  <div class="flex flex-col p-4 space-y-8 text-left home">
-    <div class="flex flex-col space-y-4 w-full">
-      <code class="text-primary-accent">Better call</code>
-      <h1 class="text-6xl font-space-mono">
-        Marino Didio
-      </h1>
-      <h2 class="text-6xl font-space-mono text-primary-mid">
-        Software Developer.
-      </h2>
-      <div class="w-1/3 h-1 bg-primary-accent"></div>
-    </div>
-    <div class="w-full basis-2/3 grow text-primary-mid">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius consectetur asperiores laboriosam quisquam expedita
-      dolorum natus temporibus nihil velit delectus nesciunt, sit, obcaecati quaerat? Quam commodi deleniti doloribus
-      laborum. Corrupti.
+  <div class="flex flex-row space-x-4">
+    <!-- <div class="self-stretch basis-1 bg-primary-mid" /> -->
+    <div class="flex flex-col p-4 space-y-8 text-left home">
+      <div class="flex flex-col space-y-4 w-full">
+        <div class="flex relative space-between">
+          <code class="text-primary-accent">Better call</code>
+          <div class="absolute -top-12 -right-24 w-48 h-32 dots-pattern bg-primary-light" />
+        </div>
+
+        <h1 class="text-6xl font-space-mono">
+          Marino Didio
+        </h1>
+        <h2 class="text-6xl font-space-mono text-primary-mid">
+          Software Developer.
+        </h2>
+        <div class="w-1/3 h-1 bg-primary-light-accent"></div>
+      </div>
+      <div class="w-full basis-2/3 grow text-primary-mid">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius consectetur asperiores laboriosam quisquam expedita
+        dolorum natus temporibus nihil velit delectus nesciunt, sit, obcaecati quaerat? Quam commodi deleniti doloribus
+        laborum. Corrupti.
+      </div>
     </div>
   </div>
 </template>
 
+<style>
+.dots-pattern {
+  mask-image: url("@assets/dots-pattern.svg");
+}
+</style>
