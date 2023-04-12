e34f68d9f7fe8bea3b16c22e0f98a230177f930d
mardisen <marino.didio.work@gmail.com>
2023-04-12 12:02:32 +0200

    UI improvements + Home content

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="flex flex-row space-x-4">
-    <!-- <div class="self-stretch basis-1 bg-primary-mid" /> -->
     <div class="flex flex-col p-4 space-y-8 text-left home">
       <div class="flex flex-col space-y-4 w-full">
         <div class="flex relative space-between">
           <code class="text-primary-accent">Better call</code>
           <div class="absolute -top-12 -right-24 w-48 h-32 dots-pattern bg-primary-lightest" />
-          <div class="absolute -top-8 -right-28 w-48 h-32 dots-pattern bg-primary-dark" />
+          <div class="absolute -top-8 -right-20 w-48 h-32 mix-blend-exclusion dots-pattern bg-primary-darkest" />
         </div>
 
         <h1 class="text-6xl font-space-mono">
@@ -17,10 +16,10 @@
         </h2>
         <div class="!mt-8 w-2/3 h-1 bg-primary-light-accent"></div>
       </div>
-      <div class="w-full basis-2/3 grow text-primary-mid">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius consectetur asperiores laboriosam quisquam expedita
-        dolorum natus temporibus nihil velit delectus nesciunt, sit, obcaecati quaerat? Quam commodi deleniti doloribus
-        laborum. Corrupti.
+      <div class="w-4/5 basis-2/3 grow text-primary-mid">
+        Self-taught Software (Web) Developer, currently focusing on frontend technologies and frameworks. Coding and
+        designing with passion, while finding my place in the world.<br><br>
+        Currently seeking employement to relocate within the EU (mainly: Netherlands, Germany, Denmark, France)
       </div>
     </div>
   </div>
