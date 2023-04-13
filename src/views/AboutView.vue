56a60a675ea6dc18cd7a8e80a629a83a4e579829
mardisen <marino.didio.work@gmail.com>
2023-04-13 12:07:42 +0200

    First AboutView draft

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,32 +1,38 @@
 <template>
-  <div class="flex flex-row space-x-4">
-    <div class="flex flex-col p-4 space-y-8 text-left home">
-      <div class="flex flex-col space-y-4 w-full">
-        <div class="flex relative space-between">
-          <code class="text-primary-accent">Better call</code>
-          <div class="absolute -top-12 -right-24 w-48 h-32 dots-pattern bg-primary-lightest" />
-          <div class="absolute -top-8 -right-20 w-48 h-32 mix-blend-exclusion dots-pattern bg-primary-darkest" />
-        </div>
-
-        <h1 class="text-6xl font-space-mono">
-          Marino Didio
-        </h1>
-        <h2 class="text-6xl font-space-mono text-primary-mid">
-          Software Developer
-        </h2>
-        <div class="!mt-8 w-2/3 h-1 bg-primary-light-accent"></div>
+  <div class="flex flex-col justify-center items-center space-y-4">
+    <div class="overflow-hidden relative w-52 h-52 rounded-full basis-52 shrink-0">
+      <div
+        class="absolute w-full h-full opacity-100 mix-blend-multiply transition-colors duration-500 bg-primary-accent hover:bg-transparent" />
+      <img class="absolute -z-10"
+        src="https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?k=6&m=1223671392&s=612x612&w=0&h=NGxdexflb9EyQchqjQP0m6wYucJBYLfu46KCLNMHZYM=" />
+    </div>
+    <div class="flex flex-row items-center space-x-4 w-full">
+      <div class="h-0.5 grow bg-primary-light-accent" />
+      <h2 class="text-2xl text-primary-lightest">About me</h2>
+      <div class="h-0.5 grow bg-primary-light-accent" />
+    </div>
+    <code class="text-primary-accent !mt-0.5">01.</code>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim nam assumenda, ullam soluta facere facilis ad beatae
+      perferendis rerum quos voluptatum vitae odio pariatur iure mollitia, dolore explicabo, fuga quidem.
+    </p>
+    <div class="flex flex-row items-stretch w-full text-left justify-stretch">
+      <div class="grow">
+        <ul>
+          <li>Lorem</li>
+          <li>Ipsum</li>
+          <li>Dolor</li>
+          <li>sit</li>
+        </ul>
       </div>
-      <div class="w-4/5 basis-2/3 grow text-primary-mid">
-        Self-taught Software (Web) Developer, currently focusing on frontend technologies and frameworks. Coding and
-        designing with passion, while finding my place in the world.<br><br>
-        Currently seeking employement to relocate within the EU (mainly: Netherlands, Germany, Denmark, France)
+      <div class="grow">
+        <ul>
+          <li>Amet</li>
+          <li>Consectetur</li>
+          <li>Adipisicing</li>
+          <li>Elit</li>
+        </ul>
       </div>
     </div>
   </div>
 </template>
-
-<style>
-.dots-pattern {
-  mask-image: url("@assets/dots-pattern.svg");
-}
-</style>
